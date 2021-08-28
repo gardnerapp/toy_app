203a40bf04ab12cf68f83091407dc9c0656d282d
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-08-28 16:14:52 -0400

    Finish User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   root 'static_pages#home'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
